e2f691937ef60cc78d3ed5638a88f7266181551a
larkham <larsukyakham@gmail.comm>
2026-03-12 17:28:51 +0700

    update

--- src/services/authen.ts
+++ src/services/authen.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://172.18.9.191:5000/api', 
+  baseURL: 'http://172.18.9.175:5000/api', 
   headers: { "Content-Type": "application/json" },
 });
 
